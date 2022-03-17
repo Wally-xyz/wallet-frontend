0706a30aea7a208287d2fe9ba19c31aac79d0a22
Mayank Jain <mjmayank@gmail.com>
2022-03-17 19:19:04 -0400

    Fix state not transitioning on twitter instructions

--- src/components/Mint.tsx
+++ src/components/Mint.tsx
@@ -1,11 +1,15 @@
 import * as React from "react";
 import styled from "styled-components";
 import { SSButton as SButton } from "./Button";
+import { SSInput as SInput } from "./Input";
 
 interface Props {
     authToken: string;
-    onComplete: () => void;
 }
+
+const SFileInput = styled.input`
+    margin-bottom: 8px;
+`;
 
 const SSButton = styled(SButton)`
     &:hover {
@@ -15,14 +19,6 @@ const SSButton = styled(SButton)`
 
 const Mint = (props: Props) => {
     const [txn, setTxn] = React.useState();
-    const [imgURL, setImgURL] = React.useState('');
-    fetch(`http://localhost:80/media/recent`, {
-        headers: {
-            'Authorization': `Bearer ${props.authToken}`
-        },
-    }).then(response => response.json()).then(response => {
-        setImgURL(response.url);
-    })
     const mintNFT = async () => {
         const response = await fetch(`http://localhost:80/mint/mint`, {
             method: 'POST',
@@ -31,12 +27,10 @@ const Mint = (props: Props) => {
             },
         }).then(response => response.json()).then(response => response)
         setTxn(response.hash)
-        props.onComplete();
     }
 
     return (
         <>
-            {imgURL && <img src={imgURL}/>}
             <SSButton onClick={mintNFT}>{`Mint`}</SSButton>
             {txn && <div>{txn}</div>}
         </>
