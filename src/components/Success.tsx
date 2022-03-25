976d405cf74b38bc6b1b5a123332328975471482
Mayank <63934597+mjmayank1@users.noreply.github.com>
2022-03-24 17:56:42 -1000

    Add etherscan link to success page (#21)
    Co-authored-by: Mayank Jain <mjmayank@gmail.com>

--- src/components/Success.tsx
+++ src/components/Success.tsx
@@ -3,16 +3,19 @@ import { SSButton } from "./Button";
 
 interface Props {
     authToken: string;
+    txn: string;
     onComplete: () => void;
 }
 
 const Success = (props: Props) => {
+    const etherscan_url = `https://www.etherscan.com/tx/${props.txn}`
     return (
-        <>
-            Congrats! Your NFT has been submitted to the blockchain. This may take a couple minutes.
-            Now we'll walk you through the steps to connect it to Twitter.
+        <div>
+            <p>Congrats! Your NFT has been submitted to the blockchain. This may take a couple minutes.
+            You'll get an email when it's done, but you can also check the status <a href={etherscan_url}>here</a></p>
+            <p>Once it's minted, we'll walk you through the steps to connect it to Twitter.</p>
             <SSButton onClick={props.onComplete}>{`Connect to Twitter`}</SSButton>
-        </>
+        </div>
     );
 }
 
