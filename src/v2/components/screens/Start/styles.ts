6dc5ab4040e5ab5133056b48e0651ebafb32d874
Jyoti Puri <jyotipuri@gmail.com>
2022-06-24 11:03:14 +0530

    Adding landing page to new demo app (#64)

---
+++ src/v2/components/screens/Start/styles.ts
@@ -0,0 +1,227 @@
+import styled from "styled-components";
+import { EasyMintLogo as _EasyMintLogo } from "../../EasyMintLogo";
+import { Whale as _Whale } from "../../Whale";
+import { ButtonLink } from "../../Button";
+
+export const Button = styled(ButtonLink)`
+  margin-top: 48px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 24px 48px;
+  gap: 12px;
+
+  width: 217px;
+  height: 60px;
+
+  background: #f2f6f7;
+  border: 1px solid #fcfdfd;
+  box-shadow: 0px 8px 0px -4px #030303;
+  border-radius: 4px;
+
+  font-family: "New York";
+  font-style: normal;
+  font-weight: 810;
+  font-size: 20px;
+  line-height: 12px;
+  /* identical to box height, or 60% */
+
+  text-align: center;
+  letter-spacing: 0.0125em;
+
+  color: #0a0a0b;
+`;
+
+export const Center = styled.div`
+  margin-top: 200px;
+  align-items: flex-start;
+  display: flex;
+  flex-direction: column;
+  max-width: 620px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+`;
+
+export const Container = styled.article`
+  height: 100%;
+  width: 100%;
+  background-image: url("/landing_background.svg");
+`;
+
+export const Wrapper = styled.div`
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  height: 100%;
+  width: 100%;
+  background: radial-gradient(
+    50% 50% at 50% 50%,
+    rgba(18, 21, 23, 0.92) 0%,
+    rgba(5, 5, 5, 0.98) 100%
+  );
+`;
+
+export const EasyMintLogo = styled(_EasyMintLogo)`
+  width: 137.48px;
+  height: 32px;
+`;
+
+export const EasyMintLogoWrapper = styled.div`
+  align-self: baseline;
+  margin-left: 48px;
+  display: flex;
+  align-items: center;
+`;
+
+export const Message = styled.div`
+  font-weight: 400;
+  font-size: 20px;
+  margin-top: 28px;
+  width: 60%;
+
+  @media (max-width: 940px) {
+    text-align: center;
+  }
+`;
+
+export const TwitterInfo = styled.div`
+  margin-top: 32px;
+  width: 50%;
+  height: 48px;
+
+  font-family: "New York";
+  font-style: normal;
+  font-weight: 568;
+  font-size: 14px;
+  line-height: 24px;
+  /* or 171% */
+
+  text-align: center;
+  letter-spacing: 0.0125em;
+
+  color: #f2f6f7;
+`;
+
+export const TwitterBlueText = styled.span`
+  color: #0088ff;
+`;
+
+export const TopNav = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  width: 100%;
+  margin: 32px 32px 0 0;
+`;
+
+export const Tagline = styled.div`
+  //styleName: EasyMint/H1;
+  font-family: New York;
+  font-size: 40px;
+  font-weight: 934;
+  line-height: 56px;
+  letter-spacing: 0.0025em;
+  text-align: center;
+`;
+
+export const TopNavIconWrapper = styled.span`
+  margin-left: 10px;
+`;
+
+export const TopNavLink = styled.a`
+  display: flex;
+  align-items: center;
+  color: #cec6d2;
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 14px;
+  height: 12px;
+  line-height: 12px;
+  margin-right: 32px;
+  text-decoration: none;
+  letter-spacing: 0.0025em;
+`;
+
+export const FlexWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Footer = styled.footer`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 200px;
+`;
+
+export const FooterHeading = styled.h5`
+  height: 8px;
+
+  /* Wally/H5 */
+
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 800;
+  font-size: 12px;
+  line-height: 8px;
+  /* identical to box height, or 67% */
+
+  letter-spacing: 0.3em;
+  text-transform: uppercase;
+  margin-right: 15px;
+
+  color: #d9e3e8;
+`;
+
+export const FooterText = styled.span`
+  width: 40%;
+  height: 48px;
+
+  /* Wally/Paragraph */
+
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 24px;
+  /* or 150% */
+
+  letter-spacing: 0.0025em;
+  margin-left: 24px;
+
+  color: #dfe8ec;
+`;
+
+export const DemoBadge = styled.span`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  padding: 6px 10px 7px;
+  gap: 8px;
+
+  height: 24px;
+
+  background: #334047;
+  border-radius: 6px;
+
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 15px;
+  line-height: 11px;
+  /* identical to box height, or 73% */
+
+  color: #f2f6f7;
+
+  text-shadow: 0px 1px 0px rgba(19, 21, 22, 0.48);
+
+  margin-left: 8px;
+`;
---
+++ src/v2/icons/ExternalLink.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+
+export const ExternalLink = () => (
+  <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M13.9996 2L7.59961 8.4"
+      stroke="#A895B2"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M8.40039 2H14.0004V7.6"
+      stroke="#A895B2"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M4.45 2H2.8C2.3584 2 2 2.3584 2 2.8V13.2C2 13.6416 2.3584 14 2.8 14H13.2C13.6416 14 14 13.6416 14 13.2V11.55"
+      stroke="#A895B2"
+      strokeOpacity="0.5"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
