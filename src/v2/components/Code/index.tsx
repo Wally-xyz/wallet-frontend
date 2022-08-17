5823bce47bbfb80da01d4e57add93e870f18e257
jpuri <jyotipuri@gmail.com>
2022-08-17 21:07:53 +0530

    cleanup

--- src/v2/components/Code/index.tsx
+++ src/v2/components/Code/index.tsx
@@ -1,14 +1,14 @@
 import * as React from "react";
-// import SyntaxHighlighter from "react-syntax-highlighter";
-// import { dark } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { dark } from "react-syntax-highlighter/dist/esm/styles/hljs";
 import styled from "styled-components";
 
 export function Code({ code }: { code: string }) {
   return (
     <Wrapper>
-      {/* <SyntaxHighlighter language="javascript" style={dark} customStyle={{ background: "#180920" }}>
+      <SyntaxHighlighter language="javascript" style={dark} customStyle={{ background: "#180920" }}>
         {code}
-      </SyntaxHighlighter> */}
+      </SyntaxHighlighter>
     </Wrapper>
   );
 }
