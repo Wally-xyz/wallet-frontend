3de2c8a255b3fef0b05e9fa89d4d7739a3fbe83d
Max Burnside <maxburnside@gmail.com>
2022-10-05 22:41:22 -0700

    Navbar spacing updates

--- src/v2/components/TopNav/index.tsx
+++ src/v2/components/TopNav/index.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-import { ChevronDown } from "src/v2/icons/ChevronDown";
+// import { ChevronDown } from "src/v2/icons/ChevronDown";
 import { ExternalLink } from "src/v2/icons/ExternalLink";
 import { Logo } from "src/v2/icons/Logo";
 
@@ -10,19 +10,21 @@ export function TopNav({ variant = "BIG" }: { variant?: string }) {
   return (
     <Wrapper variant={variant}>
       <NavRouterLink to="/how-it-works">How It Works</NavRouterLink>
-      <NavLink href="/dummy">
+      {/*<NavLink href="/dummy">
         Demos
         <IconWrapper>
           <ChevronDown />
         </IconWrapper>
-      </NavLink>
+      </NavLink>*/}
       <NavLink target="_blank" href="https://docs.wally.xyz">
         Docs
         <IconWrapper>
           <ExternalLink />
         </IconWrapper>
       </NavLink>
-      <Logo />
+      <NavLink target="_blank" href="https://wally.xyz">
+        <Logo />
+      </NavLink>
     </Wrapper>
   );
 }
