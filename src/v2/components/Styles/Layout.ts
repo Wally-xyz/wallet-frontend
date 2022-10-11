2a6c57e73a5bfd93ae39b6c92330e1e087590c27
Max Burnside <maxburnside@gmail.com>
2022-10-11 08:34:32 -0700

    Connect Twitter page layout fixes

--- src/v2/components/Styles/Layout.ts
+++ src/v2/components/Styles/Layout.ts
@@ -8,11 +8,27 @@ export const FlexColumn = styled.div<{ align?: string; width?: string }>`
   width: ${({ width }) => width || "auto"};
 `;
 
+export const StepFlexColumn = styled.div<{ align?: string; width?: string }>`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: ${({ align }) => align || "center"};
+  width: ${({ width }) => width || "auto"};
+`;
+
 export const FlexRow = styled.div<{ align?: string; justify?: string; width?: string }>`
   display: flex;
   justify-content: ${({ justify }) => justify || "center"};
   align-items: ${({ align }) => align || "center"};
   width: ${({ width }) => width || "auto"};
+`;
+
+export const StepFlexRow = styled.div<{ align?: string; justify?: string; width?: string }>`
+  display: flex;
+  justify-content: space-between
+  align-items: ${({ align }) => align || "center"};
+  width: ${({ width }) => width || "auto"};
+  gap: 32px;
 `;
 
 export const Separator = styled.div<{ width?: string }>`
